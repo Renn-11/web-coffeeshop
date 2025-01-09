923e4df2d97010f9e4031245c771207b10379f7a
Renn-11 <elrenohasani@gmail.com>
2025-01-09 15:19:32 +0700

    perbaikin-navbar

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,27 @@
+const navbar = document.querySelector(".navbar");
 const navbarNav = document.querySelector('.navbar .navbar-nav');
 const toggleButton = document.querySelector('.icon-menu a#menu-toggle');
+const searchButton = document.querySelector('#search');
+const searchForm = document.querySelector('.search-form');
 
-toggleButton.addEventListener('click', (e) => {
-    e.preventDefault();// mencegah halaman memuat ulang.
-    navbarNav.classList.toggle('active');
+document.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
 });
 
-//klik diluar sidebar untuk menghilangkan nav
+const toggleActive = (element, e) => {
+    e.preventDefault();
+    element.classList.toggle('active');
+};
 
-document.addEventListener('click', function (e) {
+toggleButton.addEventListener('click', (e) => toggleActive(navbarNav, e));
+searchButton.addEventListener('click', (e) => toggleActive(searchForm, e));
+
+document.addEventListener('click', (e) => {
     if (!toggleButton.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
     }
 });
-
